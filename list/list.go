9fbc77e35aa692b550471f693c7cbd9b535eda9d
Nick Chaloult <npchaloult@gmail.com>
2019-10-02 20:46:57 -0400

    Can now run & test locally w/o copying list.json to root dir

--- list/list.go
+++ list/list.go
@@ -3,14 +3,27 @@ package list
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 )
 
 // list is a representation of the contents of list.json
 var list map[string][]string
 
+var listPath string
+
+func init() {
+	runEnv := os.Getenv("RUN_ENV")
+
+	if runEnv == "prod" {
+		listPath = "list.json"
+	} else {
+		listPath = "list/list.json"
+	}
+}
+
 // ReadList returns the currently-stored grocery list --- a list of strings
 func ReadList() map[string][]string {
-	file, err := ioutil.ReadFile("list.json")
+	file, err := ioutil.ReadFile(listPath)
 	if err != nil {
 		// TODO: better error handling
 		panic(err)
@@ -30,7 +43,7 @@ func WriteList(user string, items []string) {
 		//TODO: Fix this error handling
 		panic(err)
 	}
-	err = ioutil.WriteFile("list.json", jsonAsBytes, 0644)
+	err = ioutil.WriteFile(listPath, jsonAsBytes, 0644)
 	if err != nil {
 		//TODO: Fix this error handling
 		panic(err)
--- list/list_test.go
+++ list/list_test.go
@@ -1,9 +1,26 @@
 package list
 
 import (
+	"os"
+	"path"
 	"reflect"
+	"runtime"
 	"testing"
 )
+
+// During tests, sets file path to the project root directory.
+// Otherwise, running `go test ./...` in the project root fails because
+// list.json can't be found.
+//
+// https://brandur.org/fragments/testing-go-project-root
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "..")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func TestReadList(t *testing.T) {
 	got := ReadList()
